9dd03358d3fc930036125f8aeeea463ba5865aae
Paul Colby <git@colby.id.au>
2020-06-15 20:04:41 +1000

    Correctly handle events with no hosts specified

--- index.sh
+++ index.sh
@@ -3,12 +3,13 @@
 # Optionally, the trigger event (eg scheduled CloudWatch Events) may include hosts like:
 # {"hosts": [ "example.com", "example.com:53" ]}
 function handler {
-  set -Ceuo pipefail
+  set -Ceuo pipefail --
   # Pipe to a command group, because Lambda has no /dev/fd support (otherwise we would
   # use input redirection with command substitution to access the $ARGS array instead).
   jq -jr '.hosts[]?|tostring+"\u0000"' <<< "${1:-}" | {
+    unset hosts
     while read -d '' -r host; do hosts+=("$host"); done
-    echo "Executing: cert-metrics.sh$(printf " '%s'" "${host[@]}")"
-    . cert-metrics.sh "${hosts[@]}" 3>&2 2>&1 1>&3
+    echo "Executing: cert-metrics.sh$(printf "${hosts+ '%s'}" ${hosts+"${hosts[@]}"})"
+    source cert-metrics.sh ${hosts+"${hosts[@]}"} 3>&2 2>&1 1>&3
   }
 }
